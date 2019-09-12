7d2fc2e206f47c8e6ba5f21fb6fba56066c0a3ad
mois <mose.ns@hotmail.com>
2019-09-12 23:20:12 +0200

    Exercice 4 Done

---
+++ demand.js
@@ -0,0 +1,9 @@
+/*
+Exercice 4 :
+Refaire l’exercice 2 en demandant le nom, 
+le prénom et la ville à l’utilisateur.
+*/
+
+let nom = prompt('Entrez votre nom !');
+let prenom =prompt('Entrez votre prénom !');
+let ville = prompt('Entrez votre ville !');
