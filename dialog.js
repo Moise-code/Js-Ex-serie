c7ac272d71e086a346c16b886feabef39b19455a
mois <mose.ns@hotmail.com>
2019-09-12 22:44:05 +0200

    Exos 1  2 3 Done

---
+++ dialog.js
@@ -0,0 +1,26 @@
+/*
+Exercice 1 :
+Définir une variable et l'afficher dans une boîte de dialogue.
+*/
+
+let affich = alert('Hello, it\'s me !');
+
+/*
+Exercice 2 :
+Définir trois variables : nom, prenom et ville. 
+Les afficher comme sur l'image correspondant à l'exercice.
+*/
+
+let nom = 'Doe', prénom = 'John', ville = 'New-York';
+alert(nom+'\n'+prénom+'\n'+ville);
+
+/*
+Exercice 3 :
+Afficher une boîte de dialogue permettant de saisir son prénom. 
+Afficher dans une boîte de dialogue "Bonjour, " suivi du prénom saisi.
+*/
+
+let firstName = prompt('Entrez votre prénom !');
+alert('Bonjour, ' +firstName);
+
+
