a9bc3c29abd35ce7e8dedf7cca6b157b23a9eb25
mois <mose.ns@hotmail.com>
2019-09-18 18:00:47 +0200

    Ex1 serie3 Done

---
+++ 3/Exercice1.js
@@ -0,0 +1,13 @@
+/*
+Exercice 1
+Dans le fichier HTML fourni, changer l'image au survol de celle-ci par la deuxième.
+*/
+
+let image;
+ function change(){
+    image = document.getElementById('image1').src="images/image1_2.jpg";
+ }
+
+ function lancien(){
+    image = document.getElementById('image1').src="images/image1.jpg";
+ }
