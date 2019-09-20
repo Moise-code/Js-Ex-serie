8742828ee5cdf12e565e363b2a1122f02cbcfd8c
mois <mose.ns@hotmail.com>
2019-09-20 12:22:54 +0200

    Exercice3 Done

---
+++ serie4/ex3.js
@@ -0,0 +1,17 @@
+/*
+Dans le fichier HTML fourni, changer la couleur du texte en fonction du bouton choisi.
+*/
+
+let para;
+
+function leGreen(){
+    para = document.getElementById('texte').style.color = 'green';
+}
+
+function leRouge(){
+    para = document.getElementById('texte').style.color = 'red';
+}
+
+function leBleu(){
+    para = document.getElementById('texte').style.color = 'blue';
+}
